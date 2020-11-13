a1ea916a4b4a95c0da4fdd3a596f2b87a557160b
lizk <lizk123>
2020-11-13 12:13:22 +0800

    complete listview

--- app/src/main/java/com/example/myapplication/adapter/SimpleListView.kt
+++ app/src/main/java/com/example/myapplication/adapter/SimpleListView.kt
@@ -1,21 +1,46 @@
 package com.example.myapplication.adapter
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.example.myapplication.R
+import kotlin.jvm.internal.Reflection
 
 /**
  * 简单列表显示
  */
 
-class SimpleListView : BaseAdapter()
+class SimpleListView : BaseAdapter
 {
+
+    var context: Context;
+
+    constructor(context:Context,dataList:List<Object>){
+        this.context=context
+        this.dataList = dataList;
+    }
 
     //数据列表
     var dataList:List<Object> = listOf();
 
-    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
-        TODO("Not yet implemented")
+    override fun getView(position: Int, p1: View?, p2: ViewGroup?): View {
+        //加载 layout
+        var view =View.inflate(context, R.layout.list_item,null)
+
+        //通过view 获取 控件引用
+        var textView: TextView =view.findViewById(R.id.tv_title)
+        var textView2:TextView =view.findViewById(R.id.tv_thumbnailUrl)
+        var imageView: ImageView =view.findViewById(R.id.iv_photo_main_Image)
+        //通过text设置值
+        textView.text= this.dataList[position].toString();
+        textView2.text= this.dataList[position].toString();
+        //load 图片
+       // Glide.with(context).load(arr[position].pic).into(imageView)
+        return  view
     }
 
     override fun getItem(p0: Int): Any {
